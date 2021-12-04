cee4caa9e220a6fcb7ab26bbef91afe96b49eba7
Farnaz_Taba <farnaz.taba89@gmail.com>
2021-12-04 18:11:41 +1100

    first commit

--- bikeshare.py
+++ bikeshare.py
@@ -2,6 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
+#commit No1 from REFACTORING branch
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
